19abf949484f8012cb7de1f4ba36153f142e17a3
lixing412 <365070122@qq.com>
2016-11-24 16:26:03 +0800

    个人中心

---
+++ app/src/main/java/ruanjian/xin/xiaocaidao/ui/personal/Main4.java
@@ -0,0 +1,20 @@
+package ruanjian.xin.xiaocaidao.ui.personal;
+
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+import ruanjian.xin.xiaocaidao.R;
+
+/**
+ * Created by 你的账户 on 2016/11/23.
+ */
+
+public class Main4 extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.layout_four);
+    }
+}
