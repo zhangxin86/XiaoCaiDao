9dae7fd6b83c498651d26a39ed41853fa7ad020a
lhm <634073858@qq.com>
2016-12-06 16:38:36 +0800

    关于页面的 back 点击事件

--- app/src/main/java/ruanjian/xin/xiaocaidao/ui/personal/Person_about.java
+++ app/src/main/java/ruanjian/xin/xiaocaidao/ui/personal/Person_about.java
@@ -2,8 +2,11 @@ package ruanjian.xin.xiaocaidao.ui.personal;
 
 
 import android.os.Bundle;
+import android.provider.ContactsContract;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.ImageView;
 
 import ruanjian.xin.xiaocaidao.R;
 
@@ -13,9 +16,18 @@ import ruanjian.xin.xiaocaidao.R;
 
 public class Person_about extends AppCompatActivity {
 
+    private ImageView imageView;
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.about);
+
+        imageView = (ImageView)findViewById(R.id.back);
+        imageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
     }
 }
