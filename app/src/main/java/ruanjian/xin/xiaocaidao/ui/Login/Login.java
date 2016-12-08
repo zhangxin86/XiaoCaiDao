9b224d1ebb6403162843e48f69d2d448d967904f
liuchangling5 <liuchangling1995@163.com>
2016-12-08 14:47:27 +0800

    登陆跳转progressdialog效果实现

--- app/src/main/java/ruanjian/xin/xiaocaidao/ui/Login/Login.java
+++ app/src/main/java/ruanjian/xin/xiaocaidao/ui/Login/Login.java
@@ -1,9 +1,12 @@
 package ruanjian.xin.xiaocaidao.ui.Login;
 
 import android.app.Activity;
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
 import android.util.Log;
 import android.view.View;
 import android.view.animation.Animation;
@@ -28,6 +31,7 @@ public class Login extends Activity {
     private UserDataManager mUserDataManager;//用户数据管理类
     private SharedPreferences login_sp;
     private Animation scale;
+    private ProgressDialog pd;
 
     //用户数据
     private UserData db;
@@ -48,36 +52,60 @@ public class Login extends Activity {
         all.setAnimation(scale);
         all.startAnimation(scale);
 
+
+
+
         //建立本地数据库
 
-        //给登录按钮绑定注册监听器
 
+        //绑定事件监听器，添加progressdialog
         btnlogin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
 
                 //...
-                //获取当前输入的用户名和密码
-                username=etusername.getText().toString().trim();
-                pwd=etpwd.getText().toString().trim();
-                //SharedPreferences.Editor editor =login_sp.edit();
+                pd=ProgressDialog.show(Login.this,"提示","正在登录中");
+                new Thread(new Runnable() {
+                    @Override
+                    public void run() {
+                        spandTimeMethod();//耗时的方法
+                        handler.sendEmptyMessage(0);//执行耗时的方法之后发送给handler
+                    }
+                }).start();
+
+        }
+    });
+    }
+
+    //耗时操作
+    private void spandTimeMethod(){
+        try {
+            //获取当前输入的用户名和密码
+            username=etusername.getText().toString().trim();
+            pwd=etpwd.getText().toString().trim();
+            //SharedPreferences.Editor editor =login_sp.edit();
 //                int result=mUserDataManager.findUserByNameAndPwd(username,pwd);
 //                if (result == 1){//返回1说明用户名和密码均正确
-                    //保存用户名和密码
+            //保存用户名和密码
 //                    editor.putString("USER_NAME", username);
 //                    editor.putString("PASSWORD", pwd);
 //                    editor.commit();
-
-                    Intent intent=new Intent(Login.this,Client.class);
-                    startActivity(intent);
-                    finish();
+            Thread.sleep(3000);
+            Intent intent=new Intent(Login.this,Client.class);
+            startActivity(intent);
+            finish();
 //                }else if (result == 0){
 //                    Toast.makeText(getApplicationContext(),"登陆失败！", Toast.LENGTH_SHORT).show();
 //                }
-
-            }
-        });
-
+        }catch (InterruptedException e){
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
     }
-
+    Handler handler = new Handler() {
+        @Override
+        public void handleMessage(Message msg) {// handler接收到消息后就会执行此方法
+            pd.dismiss();// 关闭ProgressDialog
+        }
+    };
 }
