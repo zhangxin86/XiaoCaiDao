02afbd4da7a4c6e33864728294b5861705a4cd72
liuchangling5 <liuchangling1995@163.com>
2016-12-13 10:17:48 +0800

    实现登录界面和注册界面的耗时操作跳转以及校验

--- app/src/main/java/ruanjian/xin/xiaocaidao/ui/Login/Sign_in.java
+++ app/src/main/java/ruanjian/xin/xiaocaidao/ui/Login/Sign_in.java
@@ -1,17 +1,21 @@
 package ruanjian.xin.xiaocaidao.ui.Login;
 
 import android.app.Activity;
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
 import android.view.View;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import ruanjian.xin.xiaocaidao.R;
-
 
 public class Sign_in extends Activity {
     private EditText etusername;
@@ -19,19 +23,28 @@ public class Sign_in extends Activity {
     private EditText etpwd2;
     private Button btnsign;
     private Animation scale;
-    private UserDataManager mUserDataManager;         //用户数据管理类
+    private LinearLayout all;
+    private UserDataManager mUserDataManager;//用户数据管理类
+    private ProgressDialog pd1;
 
-    protected void onCreate(Bundle savedInstanceState){
+
+    private TextView tv;
+
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_in);
 
         //获取控件
-        btnsign=(Button)findViewById(R.id.btnsign);
-        etusername=(EditText) findViewById(R.id.etusername);
-        etpwd=(EditText)findViewById(R.id.etpwd);
-        etpwd2=(EditText)findViewById(R.id.etpwd2);
+        all = (LinearLayout) findViewById(R.id.all);
+        btnsign = (Button) findViewById(R.id.btnsign);
+        etusername = (EditText) findViewById(R.id.etusername);
+        etpwd = (EditText) findViewById(R.id.etpwd);
+        etpwd2 = (EditText) findViewById(R.id.etpwd2);
+        tv=(TextView)findViewById(R.id.tv);
 
         scale = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.scaleanim);
+        all.setAnimation(scale);
+        all.startAnimation(scale);
         //建立本地数据库
         if (mUserDataManager == null) {
             mUserDataManager = new UserDataManager(this);
@@ -43,50 +56,73 @@ public class Sign_in extends Activity {
             @Override
             public void onClick(View v) {
 
+                pd1 = ProgressDialog.show(Sign_in.this, "提示", "正在注册中,请稍后");
+
+                new Thread(new Runnable() {
+                    @Override
+                    public void run() {
+                        try {
+                            Thread.sleep(3000);
+                            switch (register_check()){
+                                case 1:
+                                    Message msg=handler.obtainMessage();
+                                    msg.what=1;
+                                    handler.sendMessage(msg);
+                                    break;
+                                case 2:
+                                    handler.sendEmptyMessage(2);
+                                    break;
+                                case 3:
+                                    handler.sendEmptyMessage(3);
+                                    break;
+                                case 4:
+                                    handler.sendEmptyMessage(4);
+                                    break;
+                            }
+                        }catch (InterruptedException e){
+                            e.printStackTrace();
+                        }
+
+                    }
+                }).start();
                 //..
-                register_check();
 
-
-                Intent i = new Intent(Sign_in.this,success.class);
             }
         });
 
     }
+
     //确认按钮的监听事件
-    public void register_check(){
-        if(isUserNameAndPwdValid()){
-            String username=etusername.getText().toString().trim();
-            String pwd1=etpwd.getText().toString().trim();
-            String pwd2=etpwd2.getText().toString().trim();
-            //检查用户是否存在
-            int count=mUserDataManager.findUserByName(username);
-            //用户已经存在时返回，给出提示文字
-            if (count>0){
-                Toast.makeText(this,"用户名已存在！", Toast.LENGTH_SHORT).show();
-                return;
-            }
-            //两次密码输入不一样
-            if (pwd1.equals(pwd2)==false){
-                Toast.makeText(this,"密码不正确，请重新输入", Toast.LENGTH_SHORT).show();
-                return;
-            }else {
-                UserData mUser= new UserData(username,pwd1);
-                mUserDataManager.openDataBase();
-                long flag = mUserDataManager.insertUserData(mUser);//新建用户信息
-                if(flag == -1){
-                    Toast.makeText(this,"注册失败", Toast.LENGTH_SHORT).show();
-
-                }else{
-                    Toast.makeText(this,"注册成功", Toast.LENGTH_SHORT).show();
-
-                    Intent i =new Intent(Sign_in.this,First.class);
-                    startActivity(i);
-                    finish();
+    public int  register_check() {
+            if (isUserNameAndPwdValid()) {
+                String username = etusername.getText().toString().trim();
+                String pwd1 = etpwd.getText().toString().trim();
+                String pwd2 = etpwd2.getText().toString().trim();
+                //检查用户是否存在
+                int count = mUserDataManager.findUserByName(username);
+                //用户已经存在时返回，给出提示文字
+                if (count > 0) {
+                    return 1;
+                }
+                //两次密码输入不一样
+                if (pwd1.equals(pwd2) == false) {
+                    return 2;
+                }
+                else {
+                    UserData mUser = new UserData(username, pwd1);
+                    mUserDataManager.openDataBase();
+                    long flag = mUserDataManager.insertUserData(mUser);//新建用户信息
+                    if (flag == -1) {
+                        return 3;
+                    }
+                    else {
+                        Intent i = new Intent(Sign_in.this, success.class);
+                        startActivity(i);
+                        finish();
+                    }
                 }
             }
-
-        }
-
+        return 0;
     }
     public boolean isUserNameAndPwdValid(){
         if (etusername.getText().toString().trim().equals("")){
@@ -101,4 +137,21 @@ public class Sign_in extends Activity {
         }
         return true;
     }
+    Handler handler = new Handler() {
+        @Override
+        public void handleMessage(Message msg) {// handler接收到消息后就会执行此方法
+            switch (msg.what){
+                case 1:
+                    tv.setText("用户名已存在");
+                    break;
+                case 2:
+                    tv.setText("密码不正确，请重新输入");
+                    break;
+                case 3:
+                    tv.setText("注册失败");
+                    break;
+            }
+            pd1.dismiss();// 关闭ProgressDialog
+        }
+    };
 }
--- app/src/main/java/ruanjian/xin/xiaocaidao/ui/Login/success.java
+++ app/src/main/java/ruanjian/xin/xiaocaidao/ui/Login/success.java
@@ -1,17 +1,47 @@
 package ruanjian.xin.xiaocaidao.ui.Login;
 
 import android.app.Activity;
+import android.app.ProgressDialog;
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
+
+import java.util.Timer;
+import java.util.TimerTask;
 
 import ruanjian.xin.xiaocaidao.R;
 
-/**
- * Created by 贾紫璇 on 2016/11/16.
- */
 public class success extends Activity {
+    private ProgressDialog pd;
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.success);
 
+
+        Timer timer=new Timer();
+        pd=ProgressDialog.show(success.this,"提示","正在为您自动跳转至登录页");
+        TimerTask task=new TimerTask() {
+            @Override
+            public void run() {
+                jump();
+                handler.sendEmptyMessage(0);
+                finish();
+            }
+        };
+        timer.schedule(task,1000*3);
     }
+
+    public void  jump(){
+        final Intent i=new Intent(this,First.class);
+        startActivity(i);
+    }
+
+    Handler handler = new Handler() {
+        @Override
+        public void handleMessage(Message msg) {
+            pd.dismiss();
+            // handler接收到消息后就会执行此方法
+        }
+    };
 }
